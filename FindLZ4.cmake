2ca85a71176a6b7e155c86ef3e8a8c01b7541e83
ZhuSenlin <wumuzi520@126.com>
2018-03-06 23:41:44 +0800

    bugfix

--- FindLZ4.cmake
+++ FindLZ4.cmake
@@ -1,18 +1,18 @@
 #
 # Find LZ4
 #
-#  LIBLZ4_INCLUDE_DIR - where to find lz4.h, etc.
-#  LIBLZ4_LIBRARY     - List of libraries when using liblz4.
-#  LIBLZ4_FOUND       - True if liblz4 found.
+#  LZ4_INCLUDE_DIR - where to find lz4.h, etc.
+#  LZ4_LIBRARY     - List of libraries when using liblz4.
+#  LZ4_FOUND       - True if liblz4 found.
 
-FIND_PATH(LIBLZ4_INCLUDE_DIR lz4.h)
+FIND_PATH(LZ4_INCLUDE_DIR lz4.h)
 
-FIND_LIBRARY(LIBLZ4_LIBRARY NAMES lz4)
+FIND_LIBRARY(LZ4_LIBRARY NAMES lz4)
 
-# handle the QUIETLY and REQUIRED arguments and set LIBLZ4_FOUND to TRUE if
+# handle the QUIETLY and REQUIRED arguments and set LZ4_FOUND to TRUE if
 # all listed variables are TRUE
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(LZ4 DEFAULT_MSG LIBLZ4_LIBRARY LIBLZ4_INCLUDE_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(LZ4 DEFAULT_MSG LZ4_LIBRARY LZ4_INCLUDE_DIR)
 
-MARK_AS_ADVANCED(LIBLZ4_LIBRARY LIBLZ4_INCLUDE_DIR)
+MARK_AS_ADVANCED(LZ4_LIBRARY LZ4_INCLUDE_DIR)
 
